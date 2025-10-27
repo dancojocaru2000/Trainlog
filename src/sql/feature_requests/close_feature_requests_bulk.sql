8423c49cd91379aa766ecf2460d4acbd1fb34545
simfr24 <29752452+simfr24@users.noreply.github.com>
2025-10-27 15:23:50 +0100

    Integrate user language

--- src/sql/feature_requests/close_feature_requests_bulk.sql
+++
@@ -1,6 +0,0 @@
-UPDATE feature_requests
-SET
-    status = 'not_doing',
-    description = description || E'\n\n---\nMerged into #' || :target_id,
-    last_modified = NOW()
-WHERE id = ANY(:source_ids);
